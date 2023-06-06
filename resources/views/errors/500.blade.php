dfd5452101783eb2d97a81ddb29f7c09c24e925b
t1108 <takeshita.r@aggr.jp>
2023-06-06 18:23:52 +0900

    エラーの修正

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
       <div class="col col-md-offset-3 col-md-6">
         <div class="text-center"  style="color:red">
-          <p>サーバーエラーが発生しました。このエラーが続くようであれば、お手数ですがお問合せください。</p>
+          <p>サーバーエラーが発生しました。再度お試しください。</p>
           <a href="{{ route('home') }}" class="btn">
             ホームへ戻る
           </a>
